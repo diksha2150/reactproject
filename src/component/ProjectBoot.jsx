4fbe8abb01d3388286e3e91f19e7638f4530bf12
diksha2150 <112609437+diksha2150@users.noreply.github.com>
2024-02-17 00:01:39 +0530

    text change

--- src/component/ProjectBoot.jsx
+++ src/component/ProjectBoot.jsx
@@ -20,8 +20,8 @@ function ProjectBoot() {
           </div>
         </div>
         <div class="row">
-          <div class="col-6">Buy Milk</div>
-          <div class="col-4">4/10/2023</div>
+          <div class="col-6">Buy Curd</div>
+          <div class="col-4">6/10/2024</div>
           <div class="col-2">
             <button type="button" class="btn btn-danger">
               Danger
